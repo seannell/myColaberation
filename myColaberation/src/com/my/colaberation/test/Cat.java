58a2c999cb0329523ee166d6dbb36ebb981c704f
seannell <seannell@outlook.com>
2015-11-21 11:05:53 +0800

    Add entry

---
+++ myColaberation/src/com/my/colaberation/test/Cat.java
@@ -0,0 +1,13 @@
+package com.my.colaberation.test;
+
+public class Cat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a cat");
+	}
+
+}
